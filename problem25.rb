812caa3784639d7fbb3bb733d7d177dc6235a899
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-22 22:27:05 +0200

    Problem 25: 1000-digit Fibonacci number

---
+++ problem25.rb
@@ -0,0 +1,43 @@
+"""
+Problem 25: 1000-digit Fibonacci number
+
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+
+def fibs
+  Enumerator.new do |enum|
+    a, b = 1, 1
+    while true
+      enum.yield a
+      a, b = b, a+b
+    end
+  end
+end
+
+def first_fib_with_n_digits(n)
+  fibs.detect { |f| Math.log(f, 10) > n }
+end
+
+raise "Wrong!" unless first_fib_with_n_digits(2) == 144
+
+puts first_fib_with_n_digits(1000).inspect
